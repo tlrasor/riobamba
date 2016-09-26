833fc2619b44d0c30b2ed873d76fd957f671909c
Travis Rasor <travis.rasor@argodata.com>
2016-09-26 11:19:05 -0500

    Adding junit reports

--- lib/helpers/spec_helper.rb
+++ lib/helpers/spec_helper.rb
@@ -5,8 +5,11 @@ require 'rack/parser'
 require 'rack/test'
 
 reporter_options = { color: true }
+reports_dir = ENV['CIRCLE_TEST_REPORTS'] || 'tests/reports'
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options),
-                          Minitest::Reporters::MeanTimeReporter.new]
+                          Minitest::Reporters::MeanTimeReporter.new,
+                          Minitest::Reporters::JUnitReporter.new(reports_dir)
+                        ]
 
 ENV['RACK_ENV'] = 'test'
 require "./config/environment"
